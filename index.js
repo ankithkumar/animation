1b36e72dc309f40e749e39cbc6d63f4d5e813f67
ankithkumar <ankith.b01@gmail.com>
2020-08-01 02:24:12 +0530

    animation

---
+++ index.js
@@ -0,0 +1,22 @@
+// on load of window init will be called.
+window.addEventListener('load', init);
+
+function init() {
+    observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(function (entry) {
+            if (entry.isIntersecting === true) {
+                console.log('Element is fully visible in screen');
+                entry.target.classList.remove('hidden-class');
+                entry.target.classList.add('visible-class');
+            } else {
+                console.log('element is hidden!!');
+                entry.target.classList.remove('visible-class');
+                entry.target.classList.add('hidden-class');
+            }
+        })
+    });
+    let items = document.querySelectorAll('.item');
+    items.forEach(function (item) {
+        observer.observe(item);
+    })
+}
